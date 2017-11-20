9133c7e5c4279aa983db0535756eee018609f40d
Aous Naman <aous@unsw.edu.au>
2017-11-21 10:04:47 +1100

    Some scripts to make things easier

---
+++ config_routing.sh
@@ -0,0 +1,8 @@
+sudo route add -net 192.168.21.0/24 192.168.20.254
+sudo route add -net 192.168.22.0/24 192.168.20.254
+sudo route add -net 192.168.23.0/24 192.168.20.254
+sudo route add -net 192.168.24.0/24 192.168.20.254
+sudo route add -net 192.168.11.0/24 192.168.10.254
+sudo route add -net 192.168.12.0/24 192.168.10.254
+sudo route add -net 192.168.13.0/24 192.168.10.254
+sudo route add -net 192.168.14.0/24 192.168.10.254
---
+++ start_combined.sh
@@ -0,0 +1,4 @@
+ssh mininet@192.168.21.100 './combined1.py 192.168.11.0'
+ssh mininet@192.168.22.100 './combined1.py 192.168.12.0'
+ssh mininet@192.168.23.100 './combined1.py 192.168.13.0'
+ssh mininet@192.168.24.100 './combined1.py 192.168.14.0'
